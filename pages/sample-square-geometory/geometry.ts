abfc1b548d73a0b9c1627e5f029c3360d554f633
takashi <tks.srtr89@gmail.com>
2024-03-10 15:00:12 +0900

    delete

--- pages/sample-square-geometory/geometry.ts
+++ pages/sample-square-geometory/geometry.ts
@@ -2,7 +2,6 @@ const byteSize = 4 // 1つの値の容量
 export const squareVertexSize = byteSize * 8 // squareVertexArray内の各頂点ごとのスキップサイズ、頂点座標とカラー値を合わせて8個
 export const squarePositionOffset = byteSize * 0 // squareVertexArray内の頂点座標のオフセット位置
 export const squareColorOffset = byteSize * 4 // squareVertexArray内のカラー値のオフセット位置
-export const squareVertexCount = 4 // Number of vertices in the square.
 
 // prettier-ignore
 export const squareVertexArray = new Float32Array([

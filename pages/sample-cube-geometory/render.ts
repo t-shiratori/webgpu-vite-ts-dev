264dda4eb123bca21662f7e1c0f1c181dcc5b236
takashi <tks.srtr89@gmail.com>
2024-03-13 14:22:47 +0900

    update

--- pages/sample-cube-geometory/render.ts
+++ pages/sample-cube-geometory/render.ts
@@ -20,10 +20,15 @@ export const render = ({
   uniformBindGroup,
   depthTexture,
 }: TRenderArgs) => {
+  /** ユニフォームバッファのデータを更新 */
   writeUniformBufferMatrix({ uniformBuffer, GPU_DEVICE, GPU_CANVAS_CONTEXT })
 
+  /** GPUに発行されるコマンドをエンコードするためのエンコーダーを作成 */
   const commandEncoder = GPU_DEVICE.createCommandEncoder()
 
+  /**
+   * レンダリングパスを作成し、レンダリングに関する処理の実行コマンドを記録しレンダリングパスを終了する
+   */
   const renderPassDescriptor: GPURenderPassDescriptor = {
     colorAttachments: [
       // fragment.wgsl　main関数の戻り値の @location(0) に対応
@@ -41,12 +46,15 @@ export const render = ({
       depthStoreOp: 'store',
     },
   }
-
   const renderPassEncoder = commandEncoder.beginRenderPass(renderPassDescriptor)
   renderPassEncoder.setPipeline(pipeline)
   renderPassEncoder.setBindGroup(0, uniformBindGroup) // vertex.wgsl の @group(0) に対応
   renderPassEncoder.setVertexBuffer(0, verticesBuffer) // vertex.wgsl main関数の @location(0) に対応
   renderPassEncoder.draw(cubeVertexCount)
   renderPassEncoder.end()
+
+  /**
+   * レンダリングパスによって記録されたコマンドをコマンドバッファでラップしてGPUに送信する
+   */
   GPU_DEVICE.queue.submit([commandEncoder.finish()])
 }
